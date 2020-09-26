90897684df430005c6bf95bf506603a4c7a21b39
zhangyubogit <zhangyubo@kungeek.com>
2020-09-26 14:00:11 +0800

    新增代码

---
+++ huixiaodai/src/main/java/com/kungeek/huixiaodai/Financial_Services.java
@@ -0,0 +1,13 @@
+package com.kungeek.huixiaodai;
+
+/**
+ * @auther zhangyubo
+ * @create 2020-09-26-13:57
+ **/
+public class Financial_Services {
+    public static void main(String[] args) {
+        if(1 < 2){
+            System.out.println("慧小贷业务部成立2周年庆典");
+        }
+    }
+}
